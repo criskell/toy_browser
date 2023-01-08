fd2be1de22d7ac9147d7320b7b0186835b486a7f
Cristiano <cris.moraes.ds@gmail.com>
2023-01-08 19:38:01 -0300

    função pretty

--- src/dom.rs
+++ src/dom.rs
@@ -1,14 +1,58 @@
-pub struct Node {
-    pub data: NodeData,
-    pub children: Vec<Node>
-}
+use std::collections::HashMap;
 
-pub enum NodeData {
-    Text(String),
+#[derive(Debug)]
+pub enum Node {
+    Text {
+        value: String,
+    },
     Element {
         tag_name: String,
-        attributes: AttrMap
-    }
+        attributes: AttrMap,
+        children: Vec<Node>,
+    },
 }
 
 pub type AttrMap = HashMap<String, String>;
+
+impl Node {
+    pub fn markup(&self) -> String {
+        get_node_markup(self, 0)
+    }
+}
+
+fn get_node_markup(node: &Node, current_depth: i32) -> String {
+    let ident = "    ".repeat(current_depth.try_into().unwrap());
+
+    match node {
+        Node::Text { value } => ident + value,
+        Node::Element {
+            tag_name,
+            attributes,
+            children,
+        } => {
+            let attributes_markup = attributes
+                .iter()
+                .map(|(name, value)| format!("{}=\"{}\"", name, value))
+                .collect::<Vec<String>>()
+                .join(" ");
+            let attributes_markup = if attributes_markup.is_empty() {
+                "".to_owned()
+            } else {
+                " ".to_owned() + &attributes_markup
+            };
+
+            let content_markup = children
+                .iter()
+                .map(|node| get_node_markup(node, current_depth + 1))
+                .collect::<Vec<String>>()
+                .join("\n");
+
+            format!(
+                "{ident}<{name}{attrs}>\n{content}\n{ident}</{name}>",
+                name = tag_name,
+                attrs = attributes_markup,
+                content = content_markup,
+            )
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::collections::HashMap;
+use toy_browser::dom::Node;
+
 fn main() {
-    println!("Hello, world!");
+    let example_tree = Node::Element {
+        tag_name: "html".to_owned(),
+        attributes: HashMap::from([(String::from("lang"), String::from("pt_BR"))]),
+        children: vec![Node::Element {
+            tag_name: "body".to_owned(),
+            attributes: HashMap::new(),
+            children: vec![
+                Node::Text {
+                    value: String::from("Olá, mundo!"),
+                },
+                Node::Element {
+                    tag_name: String::from("p"),
+                    attributes: HashMap::new(),
+                    children: vec![Node::Text {
+                        value: String::from("Parágrafo."),
+                    }],
+                },
+                Node::Text {
+                    value: String::from("teste"),
+                },
+            ],
+        }],
+    };
+
+    println!("{}", example_tree.markup());
 }
